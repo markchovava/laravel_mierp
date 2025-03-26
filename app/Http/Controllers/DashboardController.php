67255b876435d95c018950bcf0647e8baa0347c9
Mark Chovava <markchovava@gmail.com>
2025-03-26 11:43:27 +0200

    Working on Authorisation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,30 +11,34 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
     public function indexBySubsidiaryDashboard(){
+        Log::info('indexBySubsidiaryDashboard');
         $auth = Auth::user();
         $users = User::where('subsidiary_id', $auth->subsidiary_id)->count();
         $products = Product::where('subsidiary_id', $auth->subsidiary_id)->count();
         // SALES
         $latestSales = Sales::max('created_at');
         $latestDate = Carbon::parse($latestSales)->toDateString();
-        $salesTotal = Sales::whereDate('created_at', $latestDate)
-                    ->where('subsidiary_id', $auth->subsidiary_id)
+        $salesTotal = Sales::where('subsidiary_id', $auth->subsidiary_id)
+                    ->whereDate('created_at', $latestDate)
                     ->sum('total');
         //  PURCHASE
         $latestPurchase = Purchase::max('created_at');
         $latestDate = Carbon::parse($latestPurchase)->toDateString();
-        $purchaseTotal = Purchase::whereDate('created_at', $latestDate)
-                    ->where('subsidiary_id', $auth->subsidiary_id)
+        $purchaseTotal = Purchase::where('subsidiary_id', $auth->subsidiary_id)
+                    ->whereDate('created_at', $latestDate)
                     ->sum('total');
         // EXPENSE
         $latestExpense = Expense::max('created_at');
+        Log::info('sub $latestExpense');
+        Log::info($latestExpense);
         $latestDate = Carbon::parse($latestExpense)->toDateString();
-        $expenseTotal = Expense::whereDate('created_at', $latestDate)
-                    ->where('subsidiary_id', $auth->subsidiary_id)
+        $expenseTotal = Expense::where('subsidiary_id', $auth->subsidiary_id)
+                    ->whereDate('created_at', $latestDate)
                     ->sum('total');
         return response()->json([
             'users_total' => $users,
@@ -42,6 +46,7 @@ class DashboardController extends Controller
             'sales_total' => $salesTotal,
             'purchase_total' => $purchaseTotal,
             'expense_total' => $expenseTotal,
+            'type' => 'subsidiary',
         ]);
     }
 
@@ -71,6 +76,7 @@ class DashboardController extends Controller
             'purchase_total' => $purchaseTotal,
             'expense_total' => $expenseTotal,
             'subsidiary_total' => $subsidiary,
+            'type' => 'admin',
         ]);
     }
 }
